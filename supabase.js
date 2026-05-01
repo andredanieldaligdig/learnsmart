8ff21c96049a26937191d0492ee74979aeab16b4
Andre Daniel Daligdig <andredanieldaligdig1@gmail.com>
2026-05-02 03:31:23 +0800

    haysxt bohai

--- supabase.js
+++ supabase.js
@@ -168,7 +168,13 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw error;
 }
-
+export async function sendPasswordReset(email) {
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: "https://learnsmart-wfmv.vercel.app/reset-password",
+  });
+  
+  if (error) throw error;
+}
 export async function getCurrentUser() {
   const { data } = await supabase.auth.getUser();
   return data.user;
